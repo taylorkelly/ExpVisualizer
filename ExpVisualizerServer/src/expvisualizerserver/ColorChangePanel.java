045c5c4a2d68bdf795ee5be23e09b62ed094a250
Taylor Kelly <tkelly910@gmail.com>
2011-11-14 08:55:22 -0500

    ColorChangePanel fade attempt

--- ExpVisualizerServer/src/expvisualizerserver/ColorChangePanel.java
+++ ExpVisualizerServer/src/expvisualizerserver/ColorChangePanel.java
@@ -22,13 +22,11 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-
 /**
-*
-* @author dpflan
-*
-*/
-
+ *
+ * @author dpflan
+ *
+ */
 public class ColorChangePanel extends JPanel { //holder for the activity panels
     public static final int WIDTH = ExpVisualizerServer.WIDTH;
     public static final int HEIGHT = ExpVisualizerServer.HEIGHT;
@@ -36,28 +34,28 @@ public class ColorChangePanel extends JPanel { //holder for the activity panels
     public List<Activity> activities;
     public List<ActivityColorPanel> activityPanels = new LinkedList<ActivityColorPanel>();
     public int heightAdjuster = 1;
-   
+
     public ColorChangePanel(List<Activity> activities) {
-      this.setPreferredSize(new Dimension(WIDTH, HEIGHT / 3));
-      this.setBackground(Color.BLACK);
-      this.activities = activities;
-      this.setLayout(null);
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT / 3));
+        this.setBackground(Color.BLACK);
+        this.activities = activities;
+        this.setLayout(null);
     }
 
     public void constructActivityColorPanels(ExpVisualizerServer frame) {
-      if (this.activities.size() > 0)
-        this.heightAdjuster = this.activities.size();
-      int y = 0;
-      int yDelta = (getHeight() / this.activities.size()) - 4; //for borders
-      for (Activity activity : this.activities) {
-        activityPanels.add(new ActivityColorPanel(activity, 0, y, WIDTH, yDelta, frame));
-        y = y + yDelta;
-     }
-     for (ActivityColorPanel actPanel : activityPanels) {
-       this.add(actPanel);
-     }
+        if (this.activities.size() > 0)
+            this.heightAdjuster = this.activities.size();
+        int y = 0;
+        int yDelta = (getHeight() / this.activities.size()) - 4; //for borders
+        for (Activity activity : this.activities) {
+            activityPanels.add(new ActivityColorPanel(activity, 0, y, WIDTH, yDelta, frame));
+            y = y + yDelta;
+        }
+        for (ActivityColorPanel actPanel : activityPanels) {
+            this.add(actPanel);
+        }
     }
-    
+
     public void paintComponent(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
@@ -67,8 +65,15 @@ public class ColorChangePanel extends JPanel { //holder for the activity panels
         int height = getHeight();
         g2d.setPaint(BACKGROUND_COLOR);
         g2d.fillRect(0, 0, width, height);
+
+        double percent = 1 - Math.abs(this.getBounds().getX()) / (double) WIDTH;
+        g2d.setComposite(makeComposite((float) percent));
+        g2d.setPaint(Color.BLACK);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
     }
 
+    private AlphaComposite makeComposite(float alpha) {
+        int type = AlphaComposite.SRC_OVER;
+        return (AlphaComposite.getInstance(type, alpha));
+    }
 }
-
-
