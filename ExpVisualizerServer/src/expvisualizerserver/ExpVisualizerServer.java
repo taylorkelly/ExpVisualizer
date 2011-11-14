936ee6e23e2feb9c6a237af7d3d544f294e02358
Taylor Kelly <tkelly910@gmail.com>
2011-11-14 09:31:18 -0500

    Left/Right keys in case mouse fitzes up

--- ExpVisualizerServer/src/expvisualizerserver/ExpVisualizerServer.java
+++ ExpVisualizerServer/src/expvisualizerserver/ExpVisualizerServer.java
@@ -9,6 +9,7 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -23,6 +24,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.Component;
+import java.awt.event.KeyListener;
 
 /**
  *
@@ -86,8 +88,35 @@ public class ExpVisualizerServer extends JFrame implements ActionListener, Mouse
         this.centerWindow();
 
 
+        this.setFocusable(true);
+        this.addKeyListener(new ArrowListener());
+        //basicPanel.setFocusable(true);
+        //basicPanel.addKeyListener(new ArrowListener());
+        
         timer.start();
         listener.start();
+    }
+    
+    
+    private class ArrowListener implements KeyListener {
+
+        @Override
+        public void keyTyped(KeyEvent ke) {
+            if(ke.getKeyChar() == 'a') {
+                slideTo(0);
+            } else if(ke.getKeyChar() == 'd') {
+                slideTo(-WIDTH);
+            }
+        }
+
+        @Override
+        public void keyPressed(KeyEvent ke) {
+        }
+
+        @Override
+        public void keyReleased(KeyEvent ke) {
+        }
+    
     }
 
     public void centerWindow() {
